20df062b709efc7f43c4d6c310a27b1d51fda3cc
Abin <abinbvinod@gmail.com>
2023-01-05 14:29:16 +0530

    apitest

---
+++ simpleapi.js
@@ -0,0 +1,10 @@
+const http =require('http');
+
+const data = require('./simpledata')
+http.createServer((req,res)=>{
+
+
+res.writeHead(200,{'content-Type':'application\json'});
+res.write(JSON.stringify(data));
+res.end();
+}).listen(6969)
---
+++ simpledata.js
@@ -0,0 +1,8 @@
+
+const data =[
+    {name:'abin',email:'abintest@gmail.com'},
+    {name:'notabin',email:'abintest1@gmail.com'},
+    {name:'abinreal',email:'abintest2@gmail.com'}
+
+]
+module.exports=data;
